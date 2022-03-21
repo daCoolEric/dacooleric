4d7933a0773cd46a168f130ac3a268acaf5d4a2c
ericy1 <eric.ayi@amalitech.org>
2022-03-21 13:58:54 +0000

    add few changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,65 +4,67 @@ import ContactCSS from "./Contact.module.css";
 function Contact() {
   return (
     <div className={ContactCSS.container}>
-      <div className={ContactCSS.content}>
-        <div>
-          <h1 className={ContactCSS.title}>Let's talk.</h1>
-        </div>
-        <div>
-          <div className={ContactCSS.contactInfo}>
-            Get in touch via the form below, or by emailing
-            <span>
-              <a href="ayieric7@gmail.com" className={ContactCSS.textColor}>
-                ayieric7@gmail.com
-              </a>
-            </span>
-            .
+      <div className={ContactCSS.contentScroll}>
+        <div className={ContactCSS.content}>
+          <div>
+            <h1 className={ContactCSS.title}>Let's talk.</h1>
           </div>
-          <form action="">
-            <label for="name" className={ContactCSS.formLabel}>
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              className={`${ContactCSS.formDisplay}
+          <div>
+            <div className={ContactCSS.contactInfo}>
+              Get in touch via the form below, or by emailing
+              <span>
+                <a href="ayieric7@gmail.com" className={ContactCSS.textColor}>
+                  ayieric7@gmail.com
+                </a>
+              </span>
+              .
+            </div>
+            <form action="">
+              <label for="name" className={ContactCSS.formLabel}>
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                className={`${ContactCSS.formDisplay}
                 ${ContactCSS.formDisplayInput}
                 ${ContactCSS.formInput}
                `}
-              // className="formDisplay form-display-input formInput"
-              placeholder="Enter your name"
-            />
-            <label for="email" className={ContactCSS.formLabel}>
-              Email Address:
-            </label>
-            <input
-              type="text"
-              className={`${ContactCSS.formDisplay}
+                // className="formDisplay form-display-input formInput"
+                placeholder="Enter your name"
+              />
+              <label for="email" className={ContactCSS.formLabel}>
+                Email Address:
+              </label>
+              <input
+                type="text"
+                className={`${ContactCSS.formDisplay}
                 ${ContactCSS.formDisplayInput}
                 ${ContactCSS.formInput}
                 ${ContactCSS.inputDisplayOnFocus}
                `}
-              placeholder="Enter your email address"
-            />
-            <label for="message" className={ContactCSS.formLabel}>
-              Message:
-            </label>
-            <textarea
-              type="text"
-              id="message"
-              className={`${ContactCSS.formDisplay}
+                placeholder="Enter your email address"
+              />
+              <label for="message" className={ContactCSS.formLabel}>
+                Message:
+              </label>
+              <textarea
+                type="text"
+                id="message"
+                className={`${ContactCSS.formDisplay}
                 ${ContactCSS.formDisplayInput}
                 ${ContactCSS.formInput}
                 ${ContactCSS.textArea}`}
-              placeholder="Enter your message"
-            ></textarea>
-            <button
-              className={`${ContactCSS.formSubmit} ${ContactCSS.formInput}`}
-              id="form-submit"
-            >
-              Submit
-            </button>
-          </form>
+                placeholder="Enter your message"
+              ></textarea>
+              <button
+                className={`${ContactCSS.formSubmit} ${ContactCSS.formInput}`}
+                id="form-submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -6,25 +6,27 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 function Works() {
   return (
     <div class={WorksCSS.container}>
-      <div class={WorksCSS.content}>
-        <div>
-          <h1 class={WorksCSS.title}>
-            From Graphic Design to Website development.
-          </h1>
-        </div>
-        <div>
-          <h3 class={WorksCSS.text}>Mostly a trip down memory lane.</h3>
-        </div>
-        <div>
-          <h4 class={WorksCSS.worksLink}>
-            <Link to="/works/details" class={WorksCSS.links}>
-              Learn more
-            </Link>
+      <div className={WorksCSS.contentScroll}>
+        <div class={WorksCSS.content}>
+          <div>
+            <h1 class={WorksCSS.title}>
+              From Graphic Design to Website development.
+            </h1>
+          </div>
+          <div>
+            <h3 class={WorksCSS.text}>Mostly a trip down memory lane.</h3>
+          </div>
+          <div>
+            <h4 class={WorksCSS.worksLink}>
+              <Link to="/works/details" class={WorksCSS.links}>
+                Learn more
+              </Link>
 
-            <ArrowForwardIosIcon className={WorksCSS.learnMore}>
-              Arrow
-            </ArrowForwardIosIcon>
-          </h4>
+              <ArrowForwardIosIcon className={WorksCSS.learnMore}>
+                Arrow
+              </ArrowForwardIosIcon>
+            </h4>
+          </div>
         </div>
       </div>
     </div>
